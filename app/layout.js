f75cfddfe9031e72e628e60115e61c4b949fe539
unknown <silvanapenarondon@gmail.com>
2025-07-04 00:45:35 -0400

    keycloak login functional

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,12 @@ import { useEffect, useState } from "react";
 import Header from "./components/MobileHeader";
 import StoreProvider from "./StoreProvider";
 import { usePathname } from "next/navigation";
+import { AuthProvider } from "@/lib/contexts/auth";
+
 export default function RootLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
   const pathname = usePathname();
-
 
   const hideLayout = ["/Login", "/Login/Register"];
 
@@ -23,29 +24,31 @@ export default function RootLayout({ children }) {
   const isLoginPage = hideLayout.includes(pathname);
 
   return (
-    <StoreProvider>
-      <html lang="en">
-        <body>
-          {isLoginPage ? (
-            <main>{children}</main>
-          ) : (
-            <div className="flex h-screen bg-gray-200">
-              <div>
-                <Sidenav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    <html lang="en">
+      <body>
+        <StoreProvider>
+          <AuthProvider>
+            {isLoginPage ? (
+              <main>{children}</main>
+            ) : (
+              <div className="flex h-screen bg-gray-200">
+                <div>
+                  <Sidenav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                </div>
+                <div className="relative flex flex-col flex-1 lg:overflow-y-auto lg:overflow-x-hidden">
+                  {isMobile && (
+                    <Header
+                      setSidebarOpen={setSidebarOpen}
+                      className="sticky top-0 bg-white border-b border-slate-200 z-30"
+                    />
+                  )}
+                  <main>{children}</main>
+                </div>
               </div>
-              <div className="relative flex flex-col flex-1 lg:overflow-y-auto lg:overflow-x-hidden">
-                {isMobile && (
-                  <Header
-                    setSidebarOpen={setSidebarOpen}
-                    className="sticky top-0 bg-white border-b border-slate-200 z-30"
-                  />
-                )}
-                <main>{children}</main>
-              </div>
-            </div>
-          )}
-        </body>
-      </html>
-    </StoreProvider>
+            )}
+          </AuthProvider>
+        </StoreProvider>
+      </body>
+    </html>
   );
 }
